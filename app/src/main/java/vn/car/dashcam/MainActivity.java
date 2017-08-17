b7beb3d7e703c93ed7ec9c7536d1155fe5650332
KyleNguyen0102 <truongsonict@gmail.com>
2017-08-17 20:32:51 +0700

    Create project dashcam

---
+++ app/src/main/java/vn/car/dashcam/MainActivity.java
@@ -0,0 +1,13 @@
+package vn.car.dashcam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
