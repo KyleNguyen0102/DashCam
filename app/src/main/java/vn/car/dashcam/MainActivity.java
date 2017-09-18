a4649246bb5d525a0d539dc568e11239c2f01d41
KyleNguyen0102 <truongsonict@gmail.com>
2017-09-18 23:15:11 +0700

    Modify module app

--- app/src/main/java/vn/car/dashcam/MainActivity.java
+++ app/src/main/java/vn/car/dashcam/MainActivity.java
@@ -1,13 +1,45 @@
 package vn.car.dashcam;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity{
+import vn.car.dashcam.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    private Button mDemoRecordVideoBtn;
+
+    private Button mDemoMapBtn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void initContentView() {
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void initViews() {
+        mDemoRecordVideoBtn = findViewById(R.id.demoRecordVideoBtn);
+        mDemoMapBtn = findViewById(R.id.demoMapBtn);
+    }
+
+    @Override
+    protected void initAction() {
+        mDemoRecordVideoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Demo Record Video", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mDemoMapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Demo Map", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
